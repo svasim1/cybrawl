f4d0d8040a91853137039f1246e18bf25b9878c4
Ahoraas <109916245+Abb-FilGal@users.noreply.github.com>
2023-10-05 09:23:00 +0200

    Tried adding walljump
    when adding the now commented code i got prompted with an update screen that said i should have a backup of the project. So here is the backup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,14 @@ public class Player : MonoBehaviour
 
     [Header("Player")]
     public Transform GroundCheck;
+    public Transform WallCheck;
     public LayerMask GroundLayer;
 
     [Header("Player Movement")]
     [SerializeField] private float speed;
     [SerializeField] private float JumpForce;
     [SerializeField] private bool IsGroundedBool;
+    [SerializeField] private bool IsTouchingWall;
     [SerializeField] private string inputNameHorizontal;
     [SerializeField] private string inputNameVertical;
     public int JumpCount;
@@ -34,6 +36,7 @@ public class Player : MonoBehaviour
         IsGroundedBool = true;
 
         GroundCheck = transform.Find("GroundCheck");
+        WallCheck = transform.Find("WallCheck");
     }
 
     private void Update()
@@ -75,8 +78,11 @@ public class Player : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision){
     if(collision.gameObject.CompareTag("Ground")){
-        IsGroundedBool = true;
-        SecondJump = true;
+        //if(collision.collider == transform.Find("GroundCheck").GetComponent<Collider2D>()){
+            Debug.Log("Grounded");
+            IsGroundedBool = true;
+            SecondJump = true;
+            //}
         }
     }
     void OnTriggerExit2D(Collider2D collision){
