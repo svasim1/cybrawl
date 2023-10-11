6109b63711100a26116acaebbd576c3c65565b66
svasim <svante.ericsson06@gmail.com>
2023-10-11 11:03:56 +0200

    Added weaponDisplay to WeaponSpawner

--- Assets/Scripts/WeaponSpawner.cs
+++ Assets/Scripts/WeaponSpawner.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class WeaponSpawner : MonoBehaviour
 {
     [SerializeField] public GameObject weaponPrefab;
+    private GameObject weaponDisplay;
+
+    private void Start()
+    {
+        weaponDisplay = Instantiate(weaponPrefab, transform.position + Vector3.up * 1f, transform.rotation, transform);
+        weaponDisplay.transform.localScale = new Vector3(1f, 4f, 1f);
+
+    }
 }
