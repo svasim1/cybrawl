9b95fa785e6015f5cfb661e30275b372c9b38ab2
Ahoraas <109916245+Abb-FilGal@users.noreply.github.com>
2023-10-24 14:52:05 +0200

    ADdded levitation pad

---
+++ Assets/Scripts/LevitationPad.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevitationPad : MonoBehaviour
+{
+
+    public float LevitationForce;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * LevitationForce * 2, ForceMode2D.Impulse);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * LevitationForce * -2, ForceMode2D.Impulse);
+        }
+    }    
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * LevitationForce , ForceMode2D.Impulse);
+        }
+    }
+
+    
+}
