4558b281bc44b74dfe89b75a9d8ba2e346ad5ea4
Ahoraas <109916245+Abb-FilGal@users.noreply.github.com>
2023-11-08 12:50:31 +0100

    Added third map to PVP_maps

--- Assets/Scripts/Gameplay/PlayerHandler.cs
+++ Assets/Scripts/Gameplay/PlayerHandler.cs
@@ -44,7 +44,7 @@ public class PlayerHandler : MonoBehaviour
     }
 
     string LvlNum(){
-        string LvlNum = Random.Range(1, 3).ToString();
+        string LvlNum = Random.Range(1, 4).ToString();
         Debug.Log("Level Number: " + LvlNum);
         return LvlNum;
     }
