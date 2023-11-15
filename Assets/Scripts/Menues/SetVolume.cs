f47cadf6c114695b93b466d93dee49861eca6eed
svasim1 <svante.ericsson06@gmail.com>
2023-11-15 11:16:26 +0100

    Improved audio

--- Assets/Scripts/Menues/SetVolume.cs
+++ Assets/Scripts/Menues/SetVolume.cs
@@ -2,31 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
-public class SetVolume : MonoBehaviour {
+public class SetVolume : MonoBehaviour
+{
     public AudioMixer mixer;
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
 
     public void Start()
     {
-        // Set the initial values of the sliders to the current volume levels
+
+        // Set the volume Levels to the values saved in the PlayerPrefs
         mixer.SetFloat("MasterVol", Mathf.Log10(PlayerPrefs.GetFloat("MasterVol", 1f)) * 20);
         mixer.SetFloat("MusicVol", Mathf.Log10(PlayerPrefs.GetFloat("MusicVol", 1f)) * 20);
         mixer.SetFloat("SFXVol", Mathf.Log10(PlayerPrefs.GetFloat("SFXVol", 1f)) * 20);
+
+
+        // Set the initial values of the sliders to the current volume levels
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVol", 1f);
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVol", 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVol", 1f);
     }
-    public void SetMaster (float sliderValue)
+    public void SetMaster(float sliderValue)
     {
         // Set the volume of the Master mixer to the value of the slider
         mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
         PlayerPrefs.SetFloat("MasterVol", sliderValue);
     }
-    public void SetMusic (float sliderValue)
+    public void SetMusic(float sliderValue)
     {
         // Set the volume of the Music mixer group to the value of the slider
         mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
         PlayerPrefs.SetFloat("MusicVol", sliderValue);
     }
 
-    public void SetSFX (float sliderValue)
+    public void SetSFX(float sliderValue)
     {
         // Set the volume of the SFX mixer group to the value of the slider
         mixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
